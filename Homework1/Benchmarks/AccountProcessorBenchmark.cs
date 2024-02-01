69d171f71ea292f24a78f5f3eb016b16e4f02565
Meliodas284 <ai.pustovoy284@gmail.com>
2024-02-01 18:37:33 +0700

    Added benchmark to AccountProcessor

---
+++ Homework1/Benchmarks/AccountProcessorBenchmark.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Fuse8_ByteMinds.SummerSchool.Domain;
+
+namespace Fuse8_ByteMinds.SummerSchool.Benchmarks;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class AccountProcessorBenchmark
+{
+	private readonly AccountProcessor _processor = new();
+	private readonly BankAccount _account = new();
+
+	[Benchmark]
+	public void Calculate()
+	{
+		_processor.Calculate(_account);
+	}
+
+	[Benchmark]
+	public void CalculatePerformed()
+	{
+		_processor.CalculatePerformed(_account);
+	}
+}
