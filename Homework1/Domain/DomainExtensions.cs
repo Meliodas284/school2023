daa03314b97875446c576160b1d468b46bd2a79d
Meliodas284 <ai.pustovoy284@gmail.com>
2024-02-01 16:34:47 +0700

    Added extension methods

--- Homework1/Domain/DomainExtensions.cs
+++ Homework1/Domain/DomainExtensions.cs
@@ -1,11 +1,7 @@
-﻿using System.Data;
-
-namespace Fuse8_ByteMinds.SummerSchool.Domain;
+﻿namespace Fuse8_ByteMinds.SummerSchool.Domain;
 
 public static class DomainExtensions
 {
-	// ToDo реализовать экстеншены
-
 	/// <summary>
 	/// Сравнивает два объекта Money
 	/// </summary>
@@ -23,4 +19,29 @@ public static class DomainExtensions
 		return !money1.IsNegative ? thisAmount.CompareTo(otherAmount) : 
 			-thisAmount.CompareTo(otherAmount);
 	}
+
+	/// <summary>
+	/// Определяет пустая ли коллекция или равна ли она null
+	/// </summary>
+	/// <param name="mass">Коллекция</param>
+	/// <returns>true: коллекция пуста или равно null; false: иначе</returns>
+	public static bool IsNullOrEmpty<T>(this IEnumerable<T>? mass) => mass is null || !mass.Any();
+
+	/// <summary>
+	/// Объединяет элементы коллекции в строку по разделителю
+	/// </summary>
+	/// <param name="mass">Коллекция</param>
+	/// <param name="delimiter">Разделитель</param>
+	/// <returns>Объединенную строку</returns>
+	public static string JoinToString<T>(this IEnumerable<T> mass, string delimiter) => 
+		string.Join(delimiter, mass);
+
+	/// <summary>
+	/// Определяет количество дней между двумя датами
+	/// </summary>
+	/// <param name="firstDate">Первая дата</param>
+	/// <param name="secondDate">Вторая дата</param>
+	/// <returns>Количество дней между датами</returns>
+	public static int DaysCountBetween(this DateTimeOffset firstDate, DateTimeOffset secondDate) =>
+		(int)(firstDate - secondDate).TotalDays;
 }
