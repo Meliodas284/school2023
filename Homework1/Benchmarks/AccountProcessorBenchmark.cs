1bee43ff245f6f3c6ec8bb689efd826e358f7f5b
Meliodas284 <ai.pustovoy284@gmail.com>
2024-02-02 13:03:29 +0700

    Added comment with benchmark result

--- Homework1/Benchmarks/AccountProcessorBenchmark.cs
+++ Homework1/Benchmarks/AccountProcessorBenchmark.cs
@@ -24,3 +24,9 @@ public class AccountProcessorBenchmark
 		_processor.CalculatePerformed(in _account);
 	}
 }
+
+// Результаты
+// |             Method |       Mean |    Error |  StdDev | Rank |   Gen0 | Allocated |
+// |------------------- |-----------:|---------:|--------:|-----:|-------:|----------:|
+// | CalculatePerformed |   833.5 ns |  7.99 ns | 7.48 ns |    1 |      - |         - |
+// |          Calculate | 2,021.7 ns | 10.17 ns | 8.49 ns |    2 | 3.2120 |    6720 B |
