269e152c19c989faa69d5e6a00d53d0f5220bf1d
Meliodas284 <ai.pustovoy284@gmail.com>
2024-02-08 21:20:21 +0700

    Added private method to check limit of api requests

--- Homework3/CurrencyApi/PublicApi/Services/CurrencyService/CurrencyService.cs
+++ Homework3/CurrencyApi/PublicApi/Services/CurrencyService/CurrencyService.cs
@@ -24,3 +24,25 @@ namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Services.CurrencyService
 			_factory = factory;
 		}
 
+		/// <summary>
+		/// Получить курс валюты по умолчанию
+		/// </summary>
+		/// <returns>Информация о валюте <see cref="Currency"/></returns>
+		public async Task<Currency?> GetCurrency()
+		{
+			await CheckRequestsLimit();
+
+			var client = _factory.CreateClient("currency");
+			var uri = $"latest?currencies={_options.DefaultCurrency}&base_currency={_options.BaseCurrency}";
+
+			var response = await client.GetAsync(uri);
+
+			if (response.IsSuccessStatusCode)
+			{
+				var result = await response.Content.ReadFromJsonAsync<ExternalApiResponseDto>();
+				return result!.Data[_options.DefaultCurrency];
+			}
+
+			throw new Exception(response.StatusCode.ToString());
+		}
+
