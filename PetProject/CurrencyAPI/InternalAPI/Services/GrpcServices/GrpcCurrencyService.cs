9f345a8f236faba7398f22ce9ea57833687291df
Meliodas284 <ai.pustovoy284@gmail.com>
2024-03-11 23:09:16 +0700

    Added gRPC service
    Добавлен currency.proto файл с описанием контракта
    Добавлен класс, реализующий методы из .proto файла

---
+++ PetProject/CurrencyAPI/InternalAPI/Services/GrpcServices/GrpcCurrencyService.cs
@@ -0,0 +1,74 @@
+﻿using Grpc.Core;
+using InternalAPI.Exceptions;
+using InternalAPI.Models.Dtos;
+using InternalAPI.Services.CachedCurrencyAPIService;
+
+namespace InternalAPI.Services.GrpcServices;
+
+/// <summary>
+/// Реализует методы gRPC сервиса валют
+/// </summary>
+public class GrpcCurrencyService : CurrencyService.CurrencyServiceBase
+{
+	private readonly ICachedCurrencyAPIService _currencyService;
+
+	/// <summary>
+	/// Конструктор, инициализирует зависимости
+	/// </summary>
+	/// <param name="currencyService">Сервис валют, использующий кэш</param>
+	public GrpcCurrencyService(ICachedCurrencyAPIService currencyService)
+    {
+        _currencyService = currencyService;
+	}
+
+	/// <summary>
+	/// Получить актуальный курс заданной валюты
+	/// </summary>
+	/// <param name="request">Запрос, содержащий код валюты</param>
+	/// <param name="context">Контекст сервера</param>
+	/// <returns>Актуальный курс валюты</returns>
+	/// <exception cref="CurrencyNotFoundException"></exception>
+	public override async Task<CurrencyResponse> Get(Request request, ServerCallContext context)
+	{
+		if (!Enum.TryParse(request.Code, out CurrencyType currencyCode))
+		{
+			throw new CurrencyNotFoundException("Нет валюты с таким кодом!");
+		}
+
+		var result = await _currencyService
+				.GetCurrentCurrencyAsync(currencyCode, context.CancellationToken);
+
+		return new CurrencyResponse
+		{
+			Code = request.Code,
+			Value = (double)result.Value
+		};
+	}
+
+	/// <summary>
+	/// Получить курс заданной валюты на заданную дату
+	/// </summary>
+	/// <param name="request">Запрос, содержащий код валюты и дату</param>
+	/// <param name="context">Контекст сервера</param>
+	/// <returns>Курс валюты на дату</returns>
+	/// <exception cref="CurrencyNotFoundException"></exception>
+	public override async Task<CurrencyOnDateResponse> GetOnDate(OnDateRequest request, ServerCallContext context)
+	{
+		if (!Enum.TryParse(request.Code, out CurrencyType currencyCode))
+		{
+			throw new CurrencyNotFoundException("Нет валюты с таким кодом!");
+		}
+
+		var date = DateOnly.FromDateTime(request.Date.ToDateTime());
+
+		var result = await _currencyService
+			.GetCurrencyOnDateAsync(currencyCode, date, context.CancellationToken);
+
+		return new CurrencyOnDateResponse
+		{
+			Code = request.Code,
+			Value = (double)result.Value,
+			Date = request.Date
+		};
+	}
+}
